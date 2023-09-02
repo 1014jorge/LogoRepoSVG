418c3a6e366c58f8ca9372ec31aa63394e0d645b
Jorge Gonzalez <jorge.1014@gmail.com>
2023-09-02 09:19:44 -0700

    updated index,shapes and  gitignore files

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const { Circle, Triangle, Square } = require('./lib/shapes');
-const nsvg = require('./lib/shapes.test');
+const svg = require('./lib/shapes.test');
 
 inquirer
-.prompt([
-    {
-        type: 'list',
-        name: 'shapes',
-        message: 'What shape would you like your logo?',
-        choices: ['Circle', 'Triangle', 'Square' ],
-    },
-    {
-        type: 'input',
-        name: 'shapecolor',
-        message: 'What color would you like the shape?',
-    },
-    {
-        type: 'input',
-        name: 'characters',
-        message: 'Pick 3 characters for your logo.',
-    },
-    {
-        type: 'input',
-        name: 'characterColor',
-        message: 'What color would you like the characters?',
-    },
-]).then((response) => {
-    console.log(response);
-})
+    .prompt([
+        {
+            type: 'list',
+            name: 'shapes',
+            message: 'What shape would you like your logo?',
+            choices: ['Circle', 'Triangle', 'Square'],
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'What color would you like the shape?',
+        },
+        {
+            type: 'input',
+            name: 'characters',
+            message: 'Pick 3 characters for your logo.',
+        },
+        {
+            type: 'input',
+            name: 'characterColor',
+            message: 'What color would you like the characters?',
+        },
+    ]).then((response) => {
+        console.log(response);
+        let shapes;
+        if (response.shapes() === 'Circle') {
+            shapes = new Circle()
+        }
+        if (response.shapes() === 'Triangle') {
+            shapes = new Triangle()
+        }
+        if (response.shapes() === 'Square') {
+            shapes = new Square()
+        }
+        shapes.setColor(response.shapeColor)
+    })
