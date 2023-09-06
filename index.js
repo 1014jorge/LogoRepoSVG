5e5f7c6a5c37b6588301996c0d4ca23e3f617bcb
Jorge Gonzalez <jorge.1014@gmail.com>
2023-09-05 18:23:39 -0700

    Updated index and shapes.js file SVG renders

--- index.js
+++ index.js
@@ -1,53 +1,62 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const { Circle, Triangle, Square } = require('./lib/shapes');
-const svg = require('./lib/shapes.test');
+const { Circle, Triangle, Square, Svg } = require('./lib/shapes');
+//const Svg = require('./lib/shapes');
 
-function generateLogo(){
+function generateLogo() {
 
-inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'shapes',
-            message: 'What shape would you like your logo?',
-            choices: ['Circle', 'Triangle', 'Square'],
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'What color would you like the shape?',
-        },
-        {
-            type: 'input',
-            name: 'characters',
-            message: 'Pick 3 characters for your logo.',
-        },
-        {
-            type: 'input',
-            name: 'characterColor',
-            message: 'What color would you like the characters?',
-        },
-    ]).then((response) => {
-        console.log(response);
-        let shapes;
-        if (response.shapes() === 'Circle') {
-            shapes = new Circle()
-        }
-        if (response.shapes() === 'Triangle') {
-            shapes = new Triangle()
-        }
-        if (response.shapes() === 'Square') {
-            shapes = new Square()
-        }
-        shapes.setColor(response.shapeColor)
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'shapes',
+                message: 'What shape would you like your logo?',
+                choices: ['Circle', 'Triangle', 'Square'],
+            },
+            {
+                type: 'input',
+                name: 'shapeColor',
+                message: 'What color would you like the shape?',
+            },
+            {
+                type: 'input',
+                name: 'characters',
+                message: 'Pick 3 characters for your logo.',
+            },
+            {
+                type: 'input',
+                name: 'characterColor',
+                message: 'What color would you like the characters?',
+            },
+        ]).then((response) => {
+            console.log(response);
+            let shapes;
+            if (response.shapes === 'Circle') {
+                shapes = new Circle();
+                console.log(shapes);
+            }
+            if (response.shapes === 'Triangle') {
+                shapes = new Triangle();
+            }
+            if (response.shapes === 'Square') {
+                shapes = new Square();
+            }
+            shapes.setColor(response.shapeColor);
+            console.log(shapes);
 
-        const Svg = new svg()
-        Svg.characters(response.characters, response.characterColor)
-        Svg.shapes(shapes)
-        
-        fs.writeFile('demo1.svg', Svg.render())
+            const svg = new Svg();
+            svg.setCharacters(response.characters, response.characterColor);
+            svg.setShapes(shapes);
 
-    })
+            fs.writeFile('demo1.svg', svg.render(), (err) => {
+                if (err) {
+                    console.log(err);
+
+                } else {
+                    console.log('SVG Created');
+                }
+            })
+
+        })
 }
 generateLogo();
